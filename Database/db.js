15cb7d0df0fc492d7d0c4d61ebb21a95f786cde6
sushil-diwakar <diwakarsushil942@gmail.com>
2023-06-10 14:20:18 +0530

    third

--- Database/db.js
+++ Database/db.js
@@ -7,7 +7,7 @@ const DB_URI = process.env.DB_URI;
 //Mongoose db connection 
 const connection=()=>{
     const uri= DB_URI;
-    mongoose.connect(uri,{
+    mongoose.connect(`mongodb+srv://sushildiwakar:Hello9453@cluster0.a9mglw0.mongodb.net/NotesApp?retryWrites=true&w=majority`,{
             useNewUrlParser: true,
             useUnifiedTopology: true
         }).then(()=>console.log('connection done')).catch('error')
