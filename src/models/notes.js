d0433fb95e03c98b899714da8a8a0ff689a29db1
diwakarsushil <sushil_diwakar@persistent.com>
2024-12-26 13:52:00 +0530

    disabled version

--- src/models/notes.js
+++ src/models/notes.js
@@ -7,7 +7,7 @@ const notesSchema = new Schema({
     id: {type: Number},
     title: {type:String, required:true},
     description: {type:String, required:true}
-})
+}, { versionKey: false });
 
 const Notes = mongoose.model('Notes', notesSchema);
 exports.Notes = Notes;
